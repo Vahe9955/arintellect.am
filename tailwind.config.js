2b546389c67b06ab53427e38c2274fb1ae441f43
Vahe9955 <vahe99551@gmail.com>
2023-06-26 12:39:27 +0400

    fixed center header items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -6,6 +7,10 @@ module.exports = {
       small: "0.8rem", // Adjust the value as per your requirements
     },
     extend: {
+      screens: {
+        tablet: { min: "768", max: "1200px" }, // Set minimum width to 1150px
+        ...defaultTheme.screens,
+      },
       colors: {
         "262B3A": "#262B3A",
         "custom-green": "#86DAA2",
