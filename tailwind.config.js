1e4c18c02db21483d13286e99a07c595574a7016
Vahe9955 <vahe99551@gmail.com>
2023-07-01 20:59:20 +0400

    fixed footer functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ module.exports = {
       small: "0.8rem", // Adjust the value as per your requirements
     },
     extend: {
-      screens: {
-        tablet: { min: "768", max: "1200px" }, // Set minimum width to 1150px
-        ...defaultTheme.screens,
-      },
       colors: {
         "262B3A": "#262B3A",
         "custom-green": "#86DAA2",
